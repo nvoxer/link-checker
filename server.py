26079334f434a1f249dbae10ddbf4eae47e0d208
nvoxer <163475042+nvoxer@users.noreply.github.com>
2024-08-12 14:49:25 -0300

    Add files via upload

--- server.py
+++ server.py
@@ -5,6 +5,7 @@ import requests
 from pytz import utc
 import time
 from datetime import datetime
+import os
 
 # base URL 
 base_url = "https://foros.3dgames.com.ar/threads/942062-ofertas-online-argentina/"
@@ -128,4 +129,5 @@ def index():
     return render_template('index.html', links=links_with_datetime)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=9000, debug=False)
+    port=int(os.getenv('PORT', 9000))
+    app.run(host='0.0.0.0', port=port, debug=False)
